296ac797e4fb00b7fd43aa95bd4bf218fb7679c4
mairambekiskakov <miskakov.07@gmail.com>
2023-05-01 10:15:21 +0600

    updated

---
+++ dart_1912_swithc_case/test/dart_1912_swithc_case_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_1912_swithc_case/dart_1912_swithc_case.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
